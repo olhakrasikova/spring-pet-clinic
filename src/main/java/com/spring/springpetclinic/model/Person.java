3173d33a173331549d88fe52852d2be6d2e4e9cf
Olha Krasikova <okrasikova09@gmail.com>
2022-11-20 14:06:50 +0100

    added POJOs

---
+++ src/main/java/com/spring/springpetclinic/model/Person.java
@@ -0,0 +1,12 @@
+package com.spring.springpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Person {
+
+    private String firstName;
+    private String lastName;
+}
---
+++ src/main/java/com/spring/springpetclinic/model/PetType.java
@@ -0,0 +1,11 @@
+package com.spring.springpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PetType {
+
+    private String name;
+}
