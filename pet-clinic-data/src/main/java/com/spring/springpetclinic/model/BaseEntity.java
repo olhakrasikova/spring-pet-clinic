eb48063e59443931395a88c4d75220de0754462c
Olha Krasikova <okrasikova09@gmail.com>
2022-11-21 21:06:14 +0100

    added base entity, closes #10

--- pet-clinic-data/src/main/java/com/spring/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/spring/springpetclinic/model/BaseEntity.java
@@ -3,10 +3,13 @@ package com.spring.springpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
-public class Person extends BaseEntity{
+public class BaseEntity implements Serializable {
 
-    private String firstName;
-    private String lastName;
+    private Long id;
+
+
 }
